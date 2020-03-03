7ab084c2afdc14697a3f6acc68cb8648f3a0765f
Tim <tipfom@hotmail.de>
2020-03-03 23:15:36 +0100

    added colors.py

---
+++ colors.py
@@ -0,0 +1,26 @@
+# colors
+china_color = "#1866b4"  # np.array([18, 141, 179]) / 255
+china_regression_color = "#5899DA"  # np.array([56, 209, 255]) / 255
+china_recovered_color = "#a1dbb1"
+
+row_color = "#596468"  # np.array([179, 98, 18]) / 255
+row_regression_color = "#848f94"  # np.array([255, 152, 51]) / 255
+row_recovered_color = "#3fa45b"
+
+piechart_colors = ["#66c2a3",  # recovered
+                   "#19A979",
+                   "#f5aa85",  # infected
+                   "#E8743B",
+                   "#bac1c4",  # dead
+                   "#848f94"]
+
+barchart_colors = [
+    china_color,  # MAINLAND_CHINA = 0
+    "#BF399E",  # WESTERN_PACIFIC_REGION = 1
+    "#E8743B",  # EUROPEAN_REGION = 2
+    "#19A979",  # SOUTH_EAST_ASIA_REGION = 3
+    "#ED4A7B",  # EASTERN_MEDITERRANEAN_REGION = 4
+    "#945ECF",  # REGION_OF_THE_AMERICANS = 5
+    "#13A4B4",  # AFRICAN_REGION = 6
+    "#6C8893",  # OTHER = 7
+]
